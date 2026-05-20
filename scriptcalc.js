463ee9d999a62b5af05ec3cc8c8d9285e856bdee
seguinalix23-oss <seguinalix23@gmail.com>
2026-05-20 13:56:01 +0200

    Initial commit

---
+++ scriptcalc.js
@@ -0,0 +1,21 @@
+// Je définis les variables des boutons pour les chiffres//
+
+const btnNum = document.querySelectorAll(".btnNum");
+
+//Je définis les variables pour les opérateurs
+
+const btnAdd = document.getElementById("+");
+const btnSubs = document.getElementById("-");
+const btnMult = document.getElementById("x");
+const btnDiv = document.getElementById("/");
+const btnCan = document.getElementById("C");
+const btnEgal = document.getElementById("=");
+const btn0 = document.getElementById("0");
+
+
+//Je transforme en chiffre les éléments des boutons//
+chiffreBtNum = btnNum.parseInt()
+
+
+
+
